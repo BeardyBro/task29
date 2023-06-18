d316a42d6c532f905a87f414fb6253a9380c3675
Andrey Vershinsky <vershinsky90@gmail.com>
2023-06-18 20:42:49 +0300

    task29

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int [] array = new int [8];
+void Random(int [] array)
+{
+    for ( int a = 0; a < array.Length; a++)
+    {
+        array[a] = new Random().Next(9);
+        Console.Write(array[a]+" ");
+    }
+}
+Random(array);
